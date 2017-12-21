e7377471997c68cde11ad27d26a76f8143cc4359
Hélio Silva <helio_almeida@hotmail.com>
2017-12-21 11:45:43 +0000

    Add files via upload

---
+++ PolEx.DB/Class1.cs
@@ -0,0 +1,21 @@
+﻿using SQLite.Net;
+using SQLite.Net.Async;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolEx.DB
+{
+    public class Class1
+    {
+        void myClassStuff()
+        {
+            string databasePath = "path";
+            var connectionFactory = new Func<SQLiteConnectionWithLock>(() => new SQLiteConnectionWithLock(new SQLitePlatformWinRT(), new SQLiteConnectionString(databasePath, storeDateTimeAsTicks: false)));
+            var asyncConnection = new SQLiteAsyncConnection(connectionFactory);
+        }
+
+    }
+}
---
+++ PolEx.DB/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("PolEx.DB")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Six Card Solutions Luxembourg")]
+[assembly: AssemblyProduct("PolEx.DB")]
+[assembly: AssemblyCopyright("Copyright © Six Card Solutions Luxembourg 2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("ec48ea4e-e7e5-4f94-982e-b5ab158ea399")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
