85733ab108c14fde84a8a921426b6c8339c24054
Hélio Silva <helio_almeida@hotmail.com>
2017-12-21 12:28:37 +0000

    Add files via upload

---
+++ PolEx.XAML/Controls/RSSItem.xaml.cs
@@ -0,0 +1,98 @@
+﻿using HTMLConverter;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Markup;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PolEx.XAML.Controls
+{
+    /// <summary>
+    /// Interaction logic for RSSItem.xaml
+    /// </summary>
+    public partial class RSSItem : UserControl
+    {
+        #region Properties
+
+        private String _Title;
+        public String Title
+        {
+            get { return this._Title; }
+            set
+            {
+                this._Title = value;
+                this.lblTitle.Content = value;
+            }
+        }
+
+        private String _Contents;
+        public String Contents
+        {
+            get { return this._Contents; }
+            set
+            {
+                this._Contents = value;
+                //lblContent.Text = value;
+
+                var xaml = string.Empty;
+                if (!string.IsNullOrEmpty(value))
+                    xaml = HtmlToXamlConverter.ConvertHtmlToXaml(value, false);
+
+                lblContent.Document = (FlowDocument)XamlReader.Parse(xaml);
+
+                //lblContent.Document = (FlowDocument)XamlReader.Parse(value);
+                //http://michaelsync.net/2009/06/09/bindable-wpf-richtext-editor-with-xamlhtml-convertor
+                //https://code.msdn.microsoft.com/windowsdesktop/Converting-between-RTF-and-aaa02a6e
+            }
+        }
+
+        // But probably should use this: http://stackoverflow.com/questions/10399400/best-way-to-read-rss-feed-in-net-using-c-sharp
+
+        private String _Link;
+        public String Link
+        {
+            get { return this._Link; }
+            set
+            {
+                this._Link = value;
+            }
+        }
+
+        #endregion
+
+        #region Constructors
+
+        public RSSItem()
+        {
+            InitializeComponent();
+        }
+
+        public RSSItem(String Title, String Contents)
+        {
+            InitializeComponent();
+
+            this.Title = Title;
+            this.Contents = Contents;
+        }
+
+        #endregion
+
+        private void UserControl_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            if(!String.IsNullOrEmpty(this.Link))
+            {
+                System.Diagnostics.Process.Start(this.Link);
+            }
+        }
+    }
+}
---
+++ PolEx.XAML/MainWindow.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using PolEx.XAML.Controls;
+
+using System.ServiceModel;
+using System.ServiceModel.Syndication;
+using System.Xml;
+using System.IO;
+
+namespace PolEx.XAML
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            LoadRSS();
+        }
+
+        private void LoadRSS()
+        {
+            try
+            {
+                XmlReaderSettings settings = new XmlReaderSettings();
+                settings.DtdProcessing = DtdProcessing.Ignore;
+
+                //string url = "http://www.jogossantacasa.pt/web/SCRss/rssFeedCartRes";
+                string url = "http://www.police.public.lu/fr/support/rss/actualites/index.html";
+
+                XmlDocument doc = new XmlDocument();
+                doc.Load("testrss.xml");
+
+                string xmlString = doc.InnerXml;
+
+                TextReader tr = new StringReader(xmlString);
+                XmlReader xmlReader = XmlReader.Create(tr);
+                SyndicationFeed feed = SyndicationFeed.Load(xmlReader);
+                xmlReader.Close();
+
+
+                // Load items on a listbox
+                List<RSSItem> lstRSS = new List<RSSItem>();
+                foreach (SyndicationItem item in feed.Items)
+                {
+                    //lstRSS.Add(new RSSItem(item.Title.Text, ReformatHTML(item.Summary.Text)));
+                    lstRSS.Add(new RSSItem(item.Title.Text, item.Summary.Text));
+                }
+                //ListRSS.ItemsSource = lstRSS;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            
+        }
+
+        private String ReformatHTML(String htmlText)
+        {
+            return htmlText.Replace("b>", "bold>");
+        }
+
+
+    }
+}
